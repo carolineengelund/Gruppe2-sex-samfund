0c8c15e827797773ed8dd7f4996ba1efba879f3a
Cathrine Mai Rasmussen <cathrinemairasmussen@sabrinaflex.kea.local>
2017-11-13 11:28:19 +0100

    clone

--- sexogsamfund.js
+++ sexogsamfund.js
@@ -1,3 +1,5 @@
+var antalKopi = 0;
+var timerKopi;
 $(window).on("load", start);
 
 function start() {
@@ -26,7 +28,6 @@ function startHistorie() {
     $("#fugl_container").addClass("fugl_walk_ind");
     $("#fugl_sprite").addClass("fugl_walkcycle");
 
-
     $("#peter_container").on("animationend", peterVinker);
 }
 
@@ -36,14 +37,10 @@ function peterVinker() {
 
     $("#peter_container").removeClass("peter_walk_ind");
     $("#peter_sprite").removeClass("peter_walkcycle");
-
     $("#peter_sprite").addClass("peter_vinker_cycle");
 
     $("#peter_sprite").on("animationend", peterFremad);
-
-
 }
-
 
 function peterFremad() {
     console.log("peterVinker");
@@ -53,5 +50,38 @@ function peterFremad() {
     $("#peter_sprite").addClass("peter_fremad_cycle");
 
     $("#peter_sprite").on("animationend", byTelefon);
+}
 
+
+
+
+
+
+
+
+
+var antalKopi = 0;
+var timerKopi;
+$(".box_btn").on("click", boxBtnHandler);
+
+function boxBtnHandler() {
+    timerKopi = setInterval(kopier, 500);
+}
+
+function kopier() {
+    console.log("kopier");
+    antalKopi++;
+    if (antalKopi <= 20) {
+        $(".box_container").clone().removeClass("box_container").addClass("box_container_kopi").appendTo("#scene").css({
+            "left": randomIntFromInterval(1, 90) + "%",
+            "top": randomIntFromInterval(1, 90) + "%"
+        });
+    } else {
+        clearInterval(timerKopi);
+        /* jeres nye function */
+    }
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
